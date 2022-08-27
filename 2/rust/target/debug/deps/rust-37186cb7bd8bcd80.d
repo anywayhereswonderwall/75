322b29d9551a14e5d54b79a0bc1c945a0c3d8b07
Michał <85255030+anywayhereswonderwall@users.noreply.github.com>
2022-08-27 20:37:31 +0200

    Delete 2/rust directory

--- 2/rust/target/debug/deps/rust-37186cb7bd8bcd80.d
+++
@@ -1,5 +0,0 @@
-/Users/michal/75/2/rust/target/debug/deps/rust-37186cb7bd8bcd80: src/main.rs
-
-/Users/michal/75/2/rust/target/debug/deps/rust-37186cb7bd8bcd80.d: src/main.rs
-
-src/main.rs:
--- 2/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/michal/75/2/rust/target/debug/rust: /Users/michal/75/2/rust/src/main.rs
