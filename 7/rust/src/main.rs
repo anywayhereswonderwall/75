2829df7aec3432e8373a9e2667a31d6404fbb81e
Michal Kaliszewski <michal.kaliszewski2@gmail.com>
2022-08-30 22:12:13 +0200

    Find Minimum in Rotated Sorted Array RUST

--- 7/rust/src/main.rs
+++ 7/rust/src/main.rs
@@ -1,3 +1,27 @@
+use std::cmp::min;
+
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![5,6,0,1,2,3];
+    println!("{:?}", solve(numbers));
+}
+
+fn solve(nums: Vec<i32>) -> i32 {
+    let mut res = nums[0];
+    let mut l = 0;
+    let mut r= nums.len() - 1;
+    while l <= r {
+        if nums[l] < nums[r] {
+            res = min(res, nums[l]);
+            break
+        }
+
+        let mid = (l + r) / 2;
+        res = min(res, nums[mid]);
+        if nums[mid] > nums[r] {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    res
 }
