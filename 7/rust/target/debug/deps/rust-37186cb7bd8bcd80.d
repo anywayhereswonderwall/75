9c1bfd9297bbf984c29b7223d215628dba4427eb
Michal Kaliszewski <michal.kaliszewski2@gmail.com>
2022-08-30 14:13:16 +0200

    rust init

---
+++ 7/rust/target/debug/deps/rust-37186cb7bd8bcd80.d
@@ -0,0 +1,5 @@
+/Users/michal/75/7/rust/target/debug/deps/rust-37186cb7bd8bcd80: src/main.rs
+
+/Users/michal/75/7/rust/target/debug/deps/rust-37186cb7bd8bcd80.d: src/main.rs
+
+src/main.rs:
---
+++ 7/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/Users/michal/75/7/rust/target/debug/rust: /Users/michal/75/7/rust/src/main.rs
