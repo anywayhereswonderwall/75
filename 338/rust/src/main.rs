54d18ec5941bfdc54e72107f1362e4a0e44a85aa
Michal Kaliszewski <michal.kaliszewski2@gmail.com>
2022-09-04 22:08:41 +0200

    Counting bits - rust

--- 338/rust/src/main.rs
+++ 338/rust/src/main.rs
@@ -6,12 +6,12 @@ fn solve(n: u32) -> Vec<i32> {
     let mut ans = vec![];
     for mut i in 0..n+1 {
         let mut count = 0;
-       while i != 0 {
-           if i % 2 == 1 {
-               count += 1;
-           }
-           i = i /2;
-       }
+        while i != 0 {
+            if i % 2 == 1 {
+                count += 1;
+            }
+            i = i /2;
+        }
         ans.push(count);
     }
     ans
